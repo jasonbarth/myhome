71b912c87e64eb13831479c04d28b29d242f3394
Jason Barth <jason.barth01@gmail.com>
2024-05-25 06:28:06 -0400

    remove vim stuff

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,8 +3,6 @@
 MYHOME=$HOME/.myhome
 echo "Using $MYHOME as bootstrap home."
 
-mkdir -p $HOME/.vim/.undo $HOME/.vim/.backup $HOME/.vim/.swp
-
 if [ -e "$HOME/.bash_profile" ]; then
 	echo ".bash_profile exists - renaming to .bash_profile_old_myhome"
 	mv "$HOME/.bash_profile" "$HOME/.bash_profile_old_myhome"
@@ -16,9 +14,3 @@ if [ -e "$HOME/.bashrc" ]; then
 	mv "$HOME/.bashrc" "$HOME/.bashrc_old_myhome"
 fi
 ln -s "$MYHOME/dotfiles/.bashrc" "$HOME/.bashrc"
-
-if [ -e "$HOME/.vimrc" ]; then
-	echo ".vimrc exists - renaming to .vimrc_old_myhome"
-	mv "$HOME/.vimrc" "$HOME/.vimrc_old_myhome"
-fi
-ln -s "$MYHOME/dotfiles/.vimrc" "$HOME/.vimrc"
